61f169422a9031665a07805ac55761d5e2db50a0
Victor Bonnelle <victor.bonnelle@protonmail.com>
2025-08-20 04:22:58 +0200

    Fix default color

--- src/storage.ts
+++ src/storage.ts
@@ -6,7 +6,7 @@ export class Storage {
     static get(id) {
         return new Promise<TData>((resolve) => {
             storage.get(id).then(data => {
-                resolve(data[id] ?? { color: "#fff", text: "" });
+                resolve(data[id] ?? { color: "#ffffff", text: "" });
             });
         });
     }
